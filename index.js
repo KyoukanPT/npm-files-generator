9b22d503d8716449873a24696d3ff4e8757dbce8
Tiago Silva <59366068+KyoukanPT@users.noreply.github.com>
2024-05-27 23:40:27 +0000

    Update index.js

--- index.js
+++ index.js
@@ -30,9 +30,3 @@ inquirer
             console.log("Something else went wrong");
         }
     });
-
-/* 
-1. Use the inquirer npm package to get user input.
-2. Use the qr-image npm package to turn the user entered URL into a QR code image.
-3. Create a txt file to save the user input using the native fs node module.
-*/
